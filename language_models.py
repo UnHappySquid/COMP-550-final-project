9808087f78293129706fc59cc54bf565bfeec058
UnHappySquid <imad.issafras@mail.mcgill.ca>
2024-12-15 15:26:16 -0500

    added simple RAG

--- language_models.py
+++ language_models.py
@@ -5,6 +5,7 @@ from langchain_core.output_parsers import JsonOutputParser
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from typing_extensions import Self
 from langchain.prompts import PromptTemplate
+from abc import ABC
 
 
 class Vehicle:
@@ -25,13 +26,16 @@ class Vehicle:
         return vehicles
 
 
-class NaiveLLM:
-    def __init__(self, vehicles: list[Vehicle]):
+class LLM(ABC):
+    template = PromptTemplate(template="Prompt:\nYou are an AI chat assistant designed to assist customers with their inquiries about the cars available at a specific dealership. You are given a list of the cars and the user's prompt.\n\nCars: \n{vehicles}\n\nUser prompt: {user_prompt}\n\nYour Response :", input_variables=[
+                              "vehicles", "user_prompt"])
+
+
+class NaiveLLM(LLM):
+    def __init__(self, vehicles: list[Vehicle], model_name: str = "llama3"):
         self.vehicles = vehicles
-        template = PromptTemplate(template="Prompt:\nYou are an AI chat assistant designed to assist customers with their inquiries about the cars available at a specific dealership. You are given a list of the cars and the user's prompt.\n\nCars: \n{vehicles}\n\nUser prompt: {user_prompt}\n\nYour Response :", input_variables=[
-                                  "vehicles", "user_prompt"])
-        llm_model = ChatOllama(model="llama3", temperature=0)
-        self.model = template | llm_model
+        llm_model = ChatOllama(model=model_name, temperature=0)
+        self.model = self.template | llm_model
 
     def prompt(self, prompt: str) -> str:
         vehicle_str = "\n".join([str(vehicle) for vehicle in self.vehicles])
@@ -39,18 +43,30 @@ class NaiveLLM:
             {"vehicles": vehicle_str, "user_prompt": prompt}).content
 
 
-class RAG:
+class SimpleRAG(LLM):
 
-    def __init__(self, vehicles: list[Vehicle]):
-        pass
+    def __init__(self, vehicles: list[Vehicle], model_name: str = "llama3",
+                 embedding_model_name: str = "llama3.2"):
+        self.vehicles = vehicles
+        vehicles_str = [str(vehicle) for vehicle in vehicles]
+        embedding_model = OllamaEmbeddings(model=embedding_model_name)
+        self.retriever = Chroma.from_texts(
+            texts=vehicles_str, collection_name="rag_vehicles",
+            embedding=embedding_model).as_retriever()
+        llm_model = ChatOllama(model=model_name, temperature=0.)
+        self.model = self.template | llm_model
 
     def prompt(self, prompt: str) -> str:
-        pass
+        docs = self.retriever.get_relevant_documents(prompt)
+        print(docs)
+        print(len(docs))
+        return self.model.invoke(
+            {"vehicles": docs, "user_prompt": prompt}).content
 
 
 def main() -> None:
     vehicles = Vehicle.load_vehicles("vehicles.txt")
-    naive_llm = NaiveLLM(vehicles)
+    naive_llm = SimpleRAG(vehicles)
     while True:
         prompt = input("> ")
         if prompt == "q":
